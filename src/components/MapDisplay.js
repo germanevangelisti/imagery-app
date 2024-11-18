19f084ada0e55f4606ddfb2447727697dd8cd972
German Evangelisti <germanevangelisti60559@gmail.com>
2024-11-18 12:39:04 -0300

    using api service function on mapDisplay

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,16 +1,12 @@
 import React from 'react';
-
-export const getGoogleMapsStaticUrl = (lat, lon, zoom = 15) => {
-    const apiKey = "AIzaSyBGAre6OmBpBX1VD3gZ8vkjA56Cch8jxP0"; // Reemplaza con tu clave de API de Google Maps
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&zoom=${zoom}&size=600x400&maptype=satellite&key=${apiKey}`;
-};
+import { getGoogleMapsStaticUrl } from '../services/apiService';
 
 function MapDisplay({ lat, lon, date }) {
     const mapUrl = getGoogleMapsStaticUrl(lat, lon);
 
     return (
         <div>
-            <h3 className='text'>Recent Imagery Capture</h3>
+            <h3 className='text'>Recentss Imagery Capture</h3>
             <p>{date ? `Date: ${new Date(date).toLocaleDateString()}` : "No date selected"}</p>
             <img src={mapUrl} alt="Satellite view of location" />
         </div>
